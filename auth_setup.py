c55a2f4d4dd7b7ac67e8273247934b3f18dbcfa5
Dan Geneau <dgeneau@csipacific.ca>
2026-06-17 10:53:48 -0700

    Add FLASK_SECRET_KEY to authentication setup

--- auth_setup.py
+++ auth_setup.py
@@ -1,6 +1,8 @@
-import os
 from dash_auth_external import DashAuthExternal
-from settings import AUTH_URL, TOKEN_URL, APP_URL, CLIENT_ID, CLIENT_SECRET
+from settings import AUTH_URL, TOKEN_URL, APP_URL, CLIENT_ID, CLIENT_SECRET, FLASK_SECRET_KEY
+
+
+stable_secret_key = FLASK_SECRET_KEY or CLIENT_SECRET
 
 auth = DashAuthExternal(
     AUTH_URL,
@@ -8,5 +10,6 @@ auth = DashAuthExternal(
     app_url=APP_URL,
     client_id=CLIENT_ID,
     client_secret=CLIENT_SECRET,
+    _secret_key=stable_secret_key,
 )
 server = auth.server  # expose the Flask server for app.py
